0cecdf6d74ed17ab55610cc12e183bd5294e9f0f
prathamesh352 <prathameshjadhav2706@gmail.com>
2024-07-23 18:23:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const port = 3000;
+const FeaturesArticles = require("./public/Featured");
+//const port = process.env.PORT;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules/bootstrap"));
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/Home", { FeaturesArticles });
+});
+
+app.get("/categories", (req, res) => {
+  res.render(__dirname + "/views/Categories");
+});
+app.get("/about", (req, res) => {
+  res.render(__dirname + "/views/About");
+});
+
+app.get("/contact", (req, res) => {
+  res.render(__dirname + "/views/Contact");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
---
+++ public/Featured.js
@@ -0,0 +1,18 @@
+const FeaturesArticles = [
+    {
+      img: "health.jpg",
+      title: "Health",
+      description: "Health is a state of complete physical mental and social well-being. ",
+    },
+    {
+      img: "sport.png",
+      title: "Sports",
+      description: "Sport is an athletic activity that involves a degree of competition. ",
+    },
+    {
+      img: "tech.png",
+      title: "Technology",
+      description: "Technology provides humans easier communication to interact friends and family."
+    },
+  ];
+  module.exports = FeaturesArticles;
